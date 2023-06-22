c6f63656d08e06fc84416e2d8ad4e1de199fefa1
Ricardo S <ricardo.supriyanto08@gmail.com>
2023-06-22 15:12:33 +0700

    fix: feedback.py

--- motor_controler/src/feedback.py
+++ motor_controler/src/feedback.py
@@ -3,6 +3,7 @@
 import rospy
 from sensor_msgs.msg import NavSatFix
 from geometry_msgs.msg import TwistStamped
+from nav_msgs.msg import Odometry
 
 def twiststamped_callback(data):
 
@@ -27,10 +28,24 @@ def navsatfix_callback(data):
 	print("NavSatFix - Longitude: {}".format(longitude))
 	print("NavSatFix - Altitude: {}".format(altitude))
 
+def odometry_callback(data):
+	position_x = data.pose.pose.position.x
+	position_y = data.pose.pose.position.y
+	position_z = data.pose.pose.position.z
+	orientation_x = data.pose.pose.orientation.x
+	orientation_y = data.pose.pose.orientation.y
+	orientation_z = data.pose.pose.orientation.z
+	orientation_w = data.pose.pose.orientation.w
+
+	print("Position: x={}, y={}, z={}".format(position_x, position_y, position_z))
+	print("Orientation: x={}, y={}, z={}, w={}".format(orientation_x, orientation_y, orientation_z, orientation_w))
+    
+
 if __name__ == '__main__':
-	rospy.init_node('feedback_pid')
+	rospy.init_node('feedback_pid', anonymous=True)
 	rospy.Subscriber('/mavros/global_position/global', NavSatFix, navsatfix_callback)
 	rospy.Subscriber('/mavros/local_position/velocity', TwistStamped, twiststamped_callback)
+	rospy.Subscriber('mavros/local_position/odom', Odometry, odometry_callback)
 	rospy.spin()
 	
 	
